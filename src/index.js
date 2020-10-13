fd8d92c4d52ecf1dc4f076199b06b8d1fbd883c5
Vadim Sidorov(2nd PC) <vadsdrv@yandex.ru>
2020-10-13 11:26:29 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let positiveNum = Math.abs(n);
+    let string = positiveNum.toString();
+    let array = string.split('');
+    array.reverse();
+    let reversedString = array.join('')
+    return reversedString;
 }
